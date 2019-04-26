e08bbf6f494c9c01c8379023f5242f1b36d1967f
Devin Zuczek <dzuczek@dlc-solutions.com>
2019-04-26 14:17:30 -0400

    Set up menu links.

--- src/Form/DenormalizerCreateForm.php
+++ src/Form/DenormalizerCreateForm.php
@@ -42,7 +42,7 @@ class DenormalizerCreateForm extends ConfirmFormBase {
    */
   public function getCancelUrl() {
     //@todo this URL needs to be updated
-    return new Url('denormalizer.create');
+    return new Url('denormalizer.settings');
   }
 
   /**
